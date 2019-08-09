da0dc18d1d22fd140166990346ad2a689380a732
DinHoo <51788868+DinHoo@users.noreply.github.com>
2019-08-09 11:42:07 -0300

    inicio

---
+++ Trabalho4-Simbiose/Assets/Scripts/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject creditsPopUp;
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Screen.SetResolution(1080, 1920, true);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void carregarJogo()
+    {
+        print("jogo");
+        //SceneManager.LoadScene("Jogo");
+    }
+
+    public void gameExit()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+
+
+    public void creditsButton(bool b)
+    {
+        creditsPopUp.SetActive(b);
+    }
+
+
+}
